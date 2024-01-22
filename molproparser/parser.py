5ea9e3437eb0b105f6f62fc49a8a37f81596fda8
ndaelman <ndaelman@physik.hu-berlin.de>
2024-01-22 19:58:08 +0100

    Add annotations and docs to the parser

--- molproparser/parser.py
+++ molproparser/parser.py
@@ -29,8 +29,21 @@ from nomad.units import ureg
 m_package = Package()
 
 
+# TODO: consider moving this to a separate file
 class MolproXMLOutParser:
+    # TODO: migrate to XML parsing tools
     def find_tags(self, tag_name: str, element=None, results=None) -> list:
+        """
+        Recursively searches for XML elements with a specific tag name.
+
+        Args:
+            tag_name (str): The name of the tag to search for.
+            element (Element, optional): The XML element to start the search from. Defaults to the root element.
+            results (list, optional): A list to store the matching elements. Defaults to an empty list.
+
+        Returns:
+            list: A list of XML elements matching the specified tag name.
+        """
         if element is None:
             element = self._root
         if results is None:
@@ -44,18 +57,39 @@ class MolproXMLOutParser:
         return results
 
     def remove_namespace(self, tree):
+        """
+        Removes the namespace from the XML tree.
+
+        Parameters:
+            tree (ElementTree.Element): The XML tree to remove the namespace from.
+
+        Returns:
+            None
+        """
         for elem in tree.iter():
             elem.tag = elem.tag.split("}")[-1]
 
     # TODO: consider storing deeply nested tags upon extraction
     def extracted_atoms(self):
+        """
+        Returns a list of extracted atoms from the parsed data.
+
+        If the list has not been extracted yet, it will be extracted using the `find_tags` method.
+
+        Returns:
+            list: A list of extracted atoms.
+        """
         if not hasattr(self, "_extracted_atoms"):
             self._extracted_atoms = self.find_tags("atom")
         return self._extracted_atoms
 
     @property
     def program(self) -> Program:
-        """Parse the program from the xml file."""
+        """Parse the program (name, version and compilation information).
+
+        Returns:
+            Program: The parsed program object.
+        """
         program: Program = Program()
         program.name = "Molpro"
         version_tag = self.find_tags("version")[0]
@@ -71,7 +105,11 @@ class MolproXMLOutParser:
 
     @property
     def atoms(self) -> Atoms:
-        """Parse the atoms from the xml file."""
+        """Parse the atoms (labels and positions).
+
+        Returns:
+            Atoms: An object representing the atoms with labels, positions, and periodicity.
+        """
         labels: list[str] = []
         positions: list[list[float]] = []
 
@@ -88,7 +126,12 @@ class MolproXMLOutParser:
     def all_atoms_group(
         self,
     ) -> AtomsGroup:  # TODO: abstract out to any kind of `System`
-        """Parse the atom indices and bonds for the entire system."""
+        """Parse the atom indices and bonds of the entire system.
+
+        Returns:
+            AtomsGroup: An object containing the parsed atom indices and bond list.
+
+        """
         convert_id = lambda x: int(x[1:])  # id-format: "a1" -> 1
         atom_indices: list[int] = []
         bond_list: list[list[int]] = []
@@ -102,7 +145,11 @@ class MolproXMLOutParser:
 
     @property
     def user_table_str(self) -> str:
-        """Parse the user table from the xml file."""
+        """Extract user-defined tables.
+
+        Returns:
+            str: The user table as an HTML string.
+        """
         user_tables = self.find_tags("table")
         if len(user_tables) > 1:
             self.logger.warning("Found more than one user table. Using the first one.")
@@ -113,7 +160,16 @@ class MolproXMLOutParser:
         return ET.tostring(user_table, encoding="unicode", method="html")
 
     def parse(self, filepath: str, archive: EntryArchive, logger) -> EntryArchive:
-        """Build up the archive from pre-defined sections."""
+        """Build up the archive from pre-defined sections.
+
+        Args:
+            filepath (str): The path to the file to be parsed.
+            archive (EntryArchive): The archive to be built up.
+            logger: The logger object for logging messages.
+
+        Returns:
+            EntryArchive: The archive with the parsed data.
+        """
         self._root = ET.parse(filepath).getroot()
         self.logger = logger
 
@@ -142,7 +198,18 @@ class MolproParser:
         self.parser = MolproXMLOutParser()
 
     def parse(self, filepath: str, archive: EntryArchive, logger) -> EntryArchive:
-        """Build up the archive from pre-defined sections."""
+        """
+        Parse the given file and build up the archive from pre-defined sections.
+        The actual parsing is delegated to the suited file-parser.
+
+        Args:
+            filepath (str): The path to the file to be parsed.
+            archive (EntryArchive): The archive to store the parsed data.
+            logger: The logger object for logging messages.
+
+        Returns:
+            EntryArchive: The updated archive with the parsed data.
+        """
         return self.parser.parse(filepath, archive, logger)
 
 
